375fd4097622cdd8dd916f2b8b50daeadfe4bfb8
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-10-20 16:59:19 -0700

    Dynamically generate products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+original = Product.create(name: "Original Cricket Flout")
+original.description = "100% Pure Cricket \n\n 100 grams(approx 10 servings)"
+original.price = 20.00
+original.save
+red_pepper = Product.create(name: "Red Pepper & Cilantro Flour")
+red_pepper.description = "100% Pure Cricket with that extra kick for those savory protein pastries \n\n 100 grams (approx 10 servings)"
+red_pepper.price = 20.00
+red_pepper.save
+apple = Product.create(name: "Apple & Cinnamon Flour")
+apple.description = "100% Pure Cricket with that added taste for your baked sweets \n\n 100 grams (approx 10 servings)"
+apple.price = 20.00
+apple.save
